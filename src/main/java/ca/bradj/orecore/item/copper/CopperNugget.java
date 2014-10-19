28c80cb6c021c0016ca4b9ff7cbc2ab863802698
bradsk88 <bradsk88@gmail.com>
2014-10-19 17:36:01 -0600

    MORE COPPER!!!

---
+++ src/main/java/ca/bradj/orecore/item/copper/CopperNugget.java
@@ -0,0 +1,13 @@
+package ca.bradj.orecore.item.copper;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class CopperNugget extends Item {
+
+	public CopperNugget(int id) {
+		this.setCreativeTab(CreativeTabs.tabMaterials);
+		setTextureName("orecore:copperNugget");
+	}
+
+}
